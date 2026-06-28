8c42b671624aecc23e308f410930af6883ff66f3
Ediga Venkata Sudheer <veedavenkat2025@gmail.com>
2026-06-28 07:45:29 +0530

    feat: setup Spring Boot backend with PostgreSQL

---
+++ backend/backend/src/test/java/com/jananijobs/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jananijobs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
